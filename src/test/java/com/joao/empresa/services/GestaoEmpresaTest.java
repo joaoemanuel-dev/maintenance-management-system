a692354794d715ae6798b92e8c68fc7b235a4914
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2026-01-13 17:43:25 -0300

    test: Valida exceção de id duplicado no cadastro de empresa

--- src/test/java/com/joao/empresa/services/GestaoEmpresaTest.java
+++ src/test/java/com/joao/empresa/services/GestaoEmpresaTest.java
@@ -1,5 +1,6 @@
 package com.joao.empresa.services;
 
+import com.joao.empresa.exceptions.EmpresaJaCadastradaException;
 import com.joao.empresa.exceptions.EmpresaNaoEncontradaException;
 import com.joao.empresa.model.Empresa;
 import org.junit.jupiter.api.Assertions;
@@ -30,6 +31,17 @@ public class GestaoEmpresaTest {
 
         Assertions.assertThrows(EmpresaNaoEncontradaException.class, () -> gestaoEmpresa.buscarPorId(2));
 
+    }
+
+    @Test
+    public void quandoOMetodoCadastrarEmpresaForChamadoSeJaExistirUmaEmpresaComOMesmoIdDeveLancarExcecao(){
+
+        GestaoEmpresa gestaoEmpresa = new GestaoEmpresa();
+        Empresa empresaNova1 = new Empresa(1, "Gerdau Açominas", "2023018977", "Ouro Branco", "Produtora de aço");
+        Empresa empresaNova2 = new Empresa(1, "Vale do Rio Doce", "98390955", "Congonhas", "Produtora de chapas");
+        gestaoEmpresa.cadastrarEmpresa(empresaNova1);
+
+        Assertions.assertThrows(EmpresaJaCadastradaException.class, () -> gestaoEmpresa.cadastrarEmpresa(empresaNova2));
     }
 
 }
