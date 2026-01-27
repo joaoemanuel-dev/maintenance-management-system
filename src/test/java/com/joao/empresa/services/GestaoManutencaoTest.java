519ecbb0fbcbb53462cbf5809d1773b97b3220f0
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2026-01-27 11:45:53 -0300

    test: valida sucesso buscarAtivasPorId

--- src/test/java/com/joao/empresa/services/GestaoManutencaoTest.java
+++ src/test/java/com/joao/empresa/services/GestaoManutencaoTest.java
@@ -1,8 +1,12 @@
 package com.joao.empresa.services;
 
 import com.joao.empresa.builders.ManutencaoBuilder;
+import com.joao.empresa.model.Equipamento;
 import com.joao.empresa.model.Manutencao;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GestaoManutencaoTest {
 
@@ -13,6 +17,16 @@ public class GestaoManutencaoTest {
     public void antesDeCadaMetodoInstanciaOObjeto(){
         gestaoManutencao = new GestaoManutencao();
         manutencaoNova = ManutencaoBuilder.builder().comId(1).build();
+    }
+
+    @Test
+    public void quandoBuscarAtivasPorIdForChamadoDeveRetornarEssaManutencao(){
+
+        gestaoManutencao.cadastrarManutencao(manutencaoNova);
+
+        Manutencao manutencao = gestaoManutencao.buscarAtivasPorId(1);
+
+        assertEquals(1, manutencao.getId());
     }
 
 
