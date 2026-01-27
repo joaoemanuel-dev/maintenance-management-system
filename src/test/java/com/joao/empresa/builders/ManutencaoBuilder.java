1193989bfd5de02fd67410f78738679e117e107d
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2026-01-27 00:32:08 -0300

    feat: adiciona ManutencaoBuilder com valores padrão para testes

---
+++ src/test/java/com/joao/empresa/builders/ManutencaoBuilder.java
@@ -0,0 +1,62 @@
+package com.joao.empresa.builders;
+
+import com.joao.empresa.model.Equipamento;
+import com.joao.empresa.model.Manutencao;
+import com.joao.empresa.model.Manutencao.*; // com esse aterisco importa tudo da classe manutenção automaticamente
+import com.joao.empresa.model.Tecnico;
+import java.time.LocalDate;
+
+public class ManutencaoBuilder {
+
+    private int id = 1;
+    private TipoManutencao tipoManutencao = TipoManutencao.CORRETIVA;
+    private LocalDate dataInicio = LocalDate.of(2026, 01, 27);
+    private String descricao = "Correia dentada da empilhadeira arrebentou";
+    private double custo = 8500.00;
+    private Tecnico tecnicoResponsavel = TecnicoBuilder.builder().build();
+    private Equipamento equipamento = EquipamentoBuilder.builder().build();
+
+    public static ManutencaoBuilder builder() {
+        return new ManutencaoBuilder();
+    }
+
+    public ManutencaoBuilder comId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public ManutencaoBuilder comTipoManutencao(TipoManutencao tipoManutencao) {
+        this.tipoManutencao = tipoManutencao;
+        return this;
+    }
+
+    public ManutencaoBuilder comDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+        return this;
+    }
+
+    public ManutencaoBuilder comDescricao(String descricao) {
+        this.descricao = descricao;
+        return this;
+    }
+
+    public ManutencaoBuilder comCusto(double custo) {
+        this.custo = custo;
+        return this;
+    }
+
+    public ManutencaoBuilder comTecnicoResponsavel(Tecnico tecnicoResponsavel) {
+        this.tecnicoResponsavel = tecnicoResponsavel;
+        return this;
+    }
+
+    public ManutencaoBuilder comEquipamento(Equipamento equipamento) {
+        this.equipamento = equipamento;
+        return this;
+    }
+
+    public Manutencao build() {
+        return new Manutencao(id, tipoManutencao, descricao, custo, tecnicoResponsavel, equipamento);
+    }
+
+}
