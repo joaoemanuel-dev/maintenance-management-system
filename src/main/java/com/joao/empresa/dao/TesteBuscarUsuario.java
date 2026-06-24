62c9961ffefdd80f8574f32b2910e56ab2c50c3b
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2026-06-24 14:32:45 -0300

    feat: implementar busca de usuário por id no UsuarioDAO com PreparedStatement e teste manual de validação

---
+++ src/main/java/com/joao/empresa/dao/TesteBuscarUsuario.java
@@ -0,0 +1,20 @@
+package com.joao.empresa.dao;
+
+import com.joao.empresa.model.Usuario;
+
+public class TesteBuscarUsuario {
+    public static void main(String[] args) {
+
+        UsuarioDAO dao = new UsuarioDAO();
+
+        Usuario usuario = dao.buscarPorId(2);
+
+        if (usuario != null) {
+            System.out.println("Nome: " + usuario.getNome());
+            System.out.println("Email: " + usuario.getEmail());
+            System.out.println("Tipo: " + usuario.getTipoUsuario());
+        } else {
+            System.out.println("Usuário não encontrado");
+        }
+    }
+}
--- src/main/java/com/joao/empresa/dao/TesteUsuarioDAO.java
+++ src/main/java/com/joao/empresa/dao/TesteUsuarioDAO.java
@@ -1,20 +1,25 @@
 package com.joao.empresa.dao;
 
-import com.joao.empresa.model.Administrador;
+import com.joao.empresa.model.Tecnico;
 import com.joao.empresa.model.Usuario;
 
 public class TesteUsuarioDAO {
     public static void main(String[] args) {
 
-        Usuario usuario = new Administrador(
+        Usuario usuario = new Tecnico(
                 0,
-                "João",
-                "joao@email.com",
-                "123456",
-                "TI"
+                "Laura",
+                "laluisa@linda",
+                "3012",
+                "Artista Plástica"
         );
 
         UsuarioDAO dao = new UsuarioDAO();
         dao.salvar(usuario);
     }
 }
+
+
+
+
+
