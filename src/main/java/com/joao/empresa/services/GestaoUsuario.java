3a76874d0b4d67870a144c24ebd48a6b29cc1f81
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2026-07-08 23:28:52 -0300

    refactor: alterar buscarPorId para consultar diretamente no banco em vez de coleção local

--- src/main/java/com/joao/empresa/services/GestaoUsuario.java
+++ src/main/java/com/joao/empresa/services/GestaoUsuario.java
@@ -1,9 +1,8 @@
 package com.joao.empresa.services;
 
 import com.joao.empresa.dao.UsuarioDAO;
-import com.joao.empresa.exceptions.UsuarioJaCadastradoException;
-import com.joao.empresa.exceptions.UsuarioNaoEncontradoException;
 import com.joao.empresa.model.Usuario;
+import com.joao.empresa.exceptions.*;
 import java.util.*;
 
 public class GestaoUsuario {
@@ -11,12 +10,13 @@ public class GestaoUsuario {
     private UsuarioDAO usuarioDAO = new UsuarioDAO(); // poder mexer no banco daqui mesmo
 
     public Usuario buscarPorId(int id){
-        return usuarios.stream()
-                .filter(usr -> usr.getId() == id)
-                .findFirst()
-                .orElseThrow(() ->
-                        new UsuarioNaoEncontradoException("Usuario com ID " + id + " não encontrado.")
-                );
+        Usuario usuario = usuarioDAO.buscarPorId(id);
+
+        if(usuario == null){
+            throw new UsuarioNaoEncontradoException("Usuario com ID \" + id + \" não encontrado.");
+        }
+
+        return usuario;
     }
 
     private Usuario buscarPorIdSemExcecao(int id) {
