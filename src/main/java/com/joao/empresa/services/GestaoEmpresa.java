c089126d14811e3accd915b0389380610d684c96
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2026-07-14 23:47:01 -0300

    refactor: ajustar método deletar para operar diretamente no banco de dados

--- src/main/java/com/joao/empresa/services/GestaoEmpresa.java
+++ src/main/java/com/joao/empresa/services/GestaoEmpresa.java
@@ -33,8 +33,8 @@ public class GestaoEmpresa {
     }
 
     public void excluirEmpresa(int id){
-        Empresa emp = buscarPorId(id);
-        empresas.remove(emp);
+        buscarPorId(id);
+        empresaDAO.deletar(id);
     }
 
 }
